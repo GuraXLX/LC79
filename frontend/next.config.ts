058e2db94ac3135bbde664960caad88b607add9c
Yashodha Gurugamage <marketing_cs@lyceum.lk>
2026-01-12 19:59:30 +0530

    feat: implement PWA

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
+// eslint-disable-next-line
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
