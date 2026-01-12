4508da29b03b136ccdb45422425beff8c884029b
Yashodha Gurugamage <marketing_cs@lyceum.lk>
2026-01-12 19:41:38 +0530

    chore: trigger deployment with api health check update

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): object {
+    return { status: 'operational', system: 'LC79 VMS API', version: '1.0.0' };
   }
 }
